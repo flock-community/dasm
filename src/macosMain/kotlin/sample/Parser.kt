ee9b7c66c13aef56ef7fcab561066bfa256f5c32
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2020-06-05 16:13:34 +0200

    TokenProvider extension functions

--- src/macosMain/kotlin/sample/Parser.kt
+++ src/macosMain/kotlin/sample/Parser.kt
@@ -17,85 +17,53 @@ class TokenProvider(private val tokenIterator: Iterator<Token>) {
     private fun nextToken() = kotlin.runCatching { tokenIterator.next() }.getOrNull()
 }
 
-
-
-
-
-typealias ProvideTokens = () -> Pair<Token?, Token?>
-typealias EatToken = () -> Unit
-
 fun List<Token>.parse(): Program {
-
-
     val tokenProvider = TokenProvider(iterator())
-
-    var nodes = mutableListOf<ProgramNode>()
-
+    val nodes = mutableListOf<ProgramNode>()
     while (tokenProvider.hasNext()) {
-
-        nodes.add(parseStatement(tokenProvider))
+        nodes.add(tokenProvider.parseStatement())
     }
-
     return nodes
 }
 
-private fun parseStatement(tokenProvider: TokenProvider): ProgramNode {
-
-    return when (tokenProvider.currentToken.type) {
-        is Keyword -> parseKeyword(tokenProvider)
-        else -> throw ParserException("Statement does not start with keyword or identifier")
-    }
+private fun TokenProvider.parseStatement(): ProgramNode = when (currentToken.type) {
+    is Keyword -> parseKeyword()
+    else -> throw ParserException("Statement does not start with keyword or identifier")
 }
 
-private fun parseKeyword(tokenProvider: TokenProvider): ProgramNode = when (tokenProvider.currentToken.value) {
-    "druk af" -> parsePrintStatement(tokenProvider)
-    "waarde" -> parseVariableDeclarationStatement(tokenProvider)
+private fun TokenProvider.parseKeyword(): ProgramNode = when (currentToken.value) {
+    "druk af" -> parsePrintStatement()
+    "waarde" -> parseVariableDeclarationStatement()
     else -> throw ParserException("Onbekend keyword")
 }
 
 
-private fun parsePrintStatement(tokenProvider: TokenProvider): ProgramNode {
-
-    tokenProvider.eatToken()
-
-    val expressionNode = parseExpression(tokenProvider)
-
-    return ProgramNode.PrintStatement(expressionNode)
+private fun TokenProvider.parsePrintStatement(): ProgramNode {
+    eatToken()
+    return ProgramNode.PrintStatement(parseExpression())
 }
 
-private fun parseExpression(tokenProvider: TokenProvider): ExpressionNode {
-    val currentToken = tokenProvider.currentToken
-    return when (currentToken.type) {
-        is Identifier -> ExpressionNode.IdentifierNode(currentToken.value)
-        is Number -> ExpressionNode.NumberNode(currentToken.value.toLong())
-        else -> throw ParserException("Onbekend token type: ${currentToken.type}")
-    }
+private fun TokenProvider.parseExpression(): ExpressionNode = when (currentToken.type) {
+    is Identifier -> ExpressionNode.IdentifierNode(currentToken.value)
+    is Number -> ExpressionNode.NumberNode(currentToken.value.toLong())
+    else -> throw ParserException("Onbekend token type: ${currentToken.type}")
 }
 
-fun parseVariableDeclarationStatement(tokenProvider: TokenProvider): ProgramNode {
-
-    // currentToken = waarde
-    tokenProvider.eatToken()
-    val variable = tokenProvider.currentToken.value
-    // currentToken = getal
-    // nextToken = wordt
-    if (tokenProvider.nextToken?.value != "wordt") throw ParserException("Assignment verwacht")
-    tokenProvider.eatToken()
-    // currentToken 14
-
-    return ProgramNode.VariableDeclaration(variable, parseExpression(tokenProvider))
+fun TokenProvider.parseVariableDeclarationStatement(): ProgramNode {
+    eatToken()
+    val variable = currentToken.value
+    if (nextToken?.value != "wordt") throw ParserException("Assignment verwacht")
+    eatToken()
+    return ProgramNode.VariableDeclaration(variable, parseExpression())
 }
 
 sealed class ProgramNode() {
-
     class PrintStatement(val expression: ExpressionNode) : ProgramNode()
     class VariableDeclaration(val name: String, val initializer: ExpressionNode) : ProgramNode()
 }
 
 sealed class ExpressionNode {
-
     class IdentifierNode(val value: String) : ExpressionNode()
     class NumberNode(val number: Long) : ExpressionNode()
-
 }
 
